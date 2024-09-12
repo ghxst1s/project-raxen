d17b4cc3fb5a66c8b568f9e3124eee1883ccd02d
ghxst1s <168595077+ghxst1s@users.noreply.github.com>
2024-09-12 07:00:14 -0700

    Update room 2395

--- rooms.js
+++ rooms.js
@@ -1,1 +1,1 @@
-{"staff": [], "private": [], "2395": ["kqrma: yooo", "mxx_xx3: sigma", "mxx_xx3: wsg"]}
+{"staff": [], "private": [], "2395": ["kqrma: yooo", "mxx_xx3: sigma", "mxx_xx3: wsg", "mxx_xx3: works good? right"]}
