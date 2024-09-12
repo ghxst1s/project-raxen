5d3539fc6b5c96adae26cbc8ce73937413546948
ghxst1s <168595077+ghxst1s@users.noreply.github.com>
2024-09-12 07:18:43 -0700

    Update room 8300

--- rooms.js
+++ rooms.js
@@ -1,1 +1,1 @@
-{"staff": [], "private": [], "6360": ["mxx_xx3: sigma", "mxx_xx3: sigma", "mxx_xx3: ahh"], "8300": ["mxx_xx3: sigma?", "mxx_xx3: toilet"]}
+{"staff": [], "private": [], "6360": ["mxx_xx3: sigma", "mxx_xx3: sigma", "mxx_xx3: ahh"], "8300": ["mxx_xx3: sigma?", "mxx_xx3: toilet", "mxx_xx3: skibidi "]}
