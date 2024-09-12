87f910c397c2a88da069f3b2db344c1aa695e7f9
ghxst1s <168595077+ghxst1s@users.noreply.github.com>
2024-09-12 07:09:11 -0700

    Update room 8364

--- rooms.js
+++ rooms.js
@@ -1,1 +1,1 @@
-{"staff": [], "private": [], "8364": ["s: hi", "mxx_xx3: hey bitch", "mxx_xx3: snicker", "s: NOTIFICATION", "s: AND FUCKING FIX THE 2 MESSAGES HOLLY SHIT"], "1814": []}
+{"staff": [], "private": [], "8364": ["s: hi", "mxx_xx3: hey bitch", "mxx_xx3: snicker", "s: NOTIFICATION", "s: AND FUCKING FIX THE 2 MESSAGES HOLLY SHIT", "mxx_xx3: bro i dont know why itts sending 2 notification"], "1814": []}
