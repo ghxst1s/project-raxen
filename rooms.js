a7888625a8a53975a0d0c9a62bc472e738bdcbbe
ghxst1s <168595077+ghxst1s@users.noreply.github.com>
2024-09-12 07:01:56 -0700

    Update room 2395

--- rooms.js
+++ rooms.js
@@ -1,1 +1,1 @@
-{"staff": [], "private": [], "2395": ["kqrma: yooo", "mxx_xx3: sigma", "mxx_xx3: wsg", "mxx_xx3: works good? right", "kqrma: :O", "kqrma: no...", "kqrma: sometimes it sends 2 times", "kqrma: check discord", "mxx_xx3: ok"]}
+{"staff": [], "private": [], "2395": ["kqrma: yooo", "mxx_xx3: sigma", "mxx_xx3: wsg", "mxx_xx3: works good? right", "kqrma: :O", "kqrma: no...", "kqrma: sometimes it sends 2 times", "kqrma: check discord", "mxx_xx3: ok", "mxx_xx3: sometimes i gotta fix it idk why its doing that"]}
