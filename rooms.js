058db8138a5c304fa60a481f0f11012289e458cc
ghxst1s <168595077+ghxst1s@users.noreply.github.com>
2024-09-12 07:08:58 -0700

    Update room 8364

--- rooms.js
+++ rooms.js
@@ -1,1 +1,1 @@
-{"staff": [], "private": [], "8364": ["s: hi", "mxx_xx3: hey bitch"], "1814": []}
+{"staff": [], "private": [], "8364": ["s: hi", "mxx_xx3: hey bitch", "mxx_xx3: snicker"], "1814": []}
