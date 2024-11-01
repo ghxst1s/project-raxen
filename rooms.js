0439a00cf8d17a980a47ace07c07f71d4b7fc333
ghxst1s <168595077+ghxst1s@users.noreply.github.com>
2024-11-01 21:07:26 +0300

    Updating rooms.js

--- rooms.js
+++ rooms.js
@@ -3,5 +3,10 @@
     "username": "mostafa1",
     "password": "x11212324x",
     "messages": "sa"
+  },
+  {
+    "username": "mostafa1",
+    "password": "x11212324x",
+    "messages": "asdada"
   }
 ]
